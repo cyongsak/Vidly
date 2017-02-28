f45cd0fb96e73c287dbda1bd4aaa4720d2656053
Yongsak Choonuan <cyongsak@hotmail.com>
2017-02-28 19:54:47 +0700

    Edit Form

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -45,11 +45,11 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel()
+            var viewModel = new CustomerFormViewModel()
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);  
+            return View("CustomerForm",viewModel);  
         }
 
         [HttpPost]
@@ -61,5 +61,18 @@ namespace Vidly.Controllers
            
         }
 
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes
+            };
+            return View("CustomerForm", viewModel);
+        }
     }
 }
